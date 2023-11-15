85ee9bf45be75cd1fa47fb6e95b7774b60fc07a8
marcofsjrtl <marco.freitas@terrenolivre.com.br>
2023-11-15 00:37:32 -0300

    Add project files.

---
+++ POO-Exemplo/Program.cs
@@ -0,0 +1,42 @@
+﻿public class Animal
+{
+    public string Name { get; set; }
+
+    public virtual string EmitirSom()
+    {
+        return "O animal emite um som";
+    }
+}
+
+public class Cachorro : Animal
+{
+    public override string EmitirSom()
+    {
+        return "O cachorro late";
+    }
+}
+
+public class Gato : Animal
+{
+    
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("O exemplo POO está iniciando");
+
+        Cachorro cachorro = new Cachorro();
+        Gato gato = new Gato();
+
+        cachorro.Name = "Rex";
+        gato.Name = "Gato";
+
+        Console.WriteLine("O nome do cachoro é " + cachorro.Name);
+        Console.WriteLine("O nome do gato é " + gato.Name);
+
+        Console.WriteLine(cachorro.EmitirSom());
+        Console.WriteLine(gato.EmitirSom());
+    }
+}
